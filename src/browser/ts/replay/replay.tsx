02bad4691690ca446a9a79bc97ba9c7f0142c3cd
Shuyang Chen <sc1085@gmail.com>
2017-02-25 04:52:06 -0600

    Wrote animate

--- src/browser/ts/replay/replay.tsx
+++ src/browser/ts/replay/replay.tsx
@@ -1,11 +1,16 @@
 import Component, { React, DataSnapshot } from '../component';
+import PlayIcon from 'react-icons/md/play-arrow';
+import StopIcon from 'react-icons/md/stop';
 
+import * as styles from '../styles';
 import Page from '../page';
 import Container from '../container';
 import Editor from '../editor';
+import Button from '../button';
 
 export interface CodeReplay {
-	[time: string]: string;
+	time: number;
+	code: string;
 }
 
 export default class Replay extends Component<{
@@ -16,8 +21,10 @@ export default class Replay extends Component<{
 	code?: string;
 	initial?: string;
 	name?: string;
-	replay?: CodeReplay;
+	replay?: CodeReplay[];
 	output?: string;
+	playing?: boolean;
+	started?: number;
 }> {
 	
 	componentWillMount() {
@@ -30,11 +37,50 @@ export default class Replay extends Component<{
 	async init() {
 		const data = await this.getReplay(this.props.params.id).once('value') as DataSnapshot;
 		const { initial, code, name } = data.val();
+		const replay = [] as CodeReplay[];
+		for (const i in code) {
+			replay.push({
+				time: parseFloat(i.replace(/\-/g, '.')),
+				code: code[i]
+			});
+		}
+		replay.sort((a, b) => a.time - b.time);
 		await this.update({
 			initial,
 			code: initial,
 			name,
-			replay: code
+			replay
+		});
+	}
+	
+	async animate() {
+		do {
+			let last = null as CodeReplay;
+			const now = this.now - this.state.started;
+			for (let i = 0; i < this.state.replay.length; i++) {
+				const replay = this.state.replay[i];
+				if (replay.time > now) {
+					break;
+				}
+				last = replay;
+			}
+			await Promise.all([this.update({
+				code: last ? last.code : this.state.initial
+			}), this.animationFrame()]);
+		} while (this.state.playing);
+	}
+	
+	async onPlay() {
+		await this.update({
+			playing: true,
+			started: this.now
+		});
+		await this.animate();
+	}
+	
+	async onStop() {
+		await this.update({
+			playing: false
 		});
 	}
 	
@@ -45,6 +91,18 @@ export default class Replay extends Component<{
 					<h3>{this.state.name}</h3>
 				</Container>
 				<Editor code={this.state.code} output={this.state.output}/>
+				<Container className="bottom">
+					<div className="editor box">
+						{this.state.playing ? 
+							<Button onClick={this.attach(this.onStop)}>
+								<StopIcon size={styles.editorIconSize}/>
+							</Button> :
+							<Button onClick={this.attach(this.onPlay)}>
+								<PlayIcon size={styles.editorIconSize}/>
+							</Button>
+						}
+					</div>
+				</Container>
 			</Page>
 		);
 	}
